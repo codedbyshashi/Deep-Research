140e31727fa5a4abdaef70d02913c71b147328e6
shashidharreddy <kotashashidharreddy1@gmail.com>
2026-09-07 14:43:08 +0530

    Added llama model

--- deep_research/config.py
+++ deep_research/config.py
@@ -59,7 +59,7 @@ nvidia_model = OpenAIChatCompletionsModel(
 )
 groq_model = OpenAIChatCompletionsModel(
     openai_client=groq_client,
-    model="openai/gpt-oss-120b",
+    model="llama-3.3-70b-versatile",
 )
 
 
